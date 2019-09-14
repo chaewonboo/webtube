64cb056f129e8ef7d0c6eb2800d197ed662098ff
채원부 <chaeoneboo@gmail.com>
2019-09-14 19:55:22 +0900

    #2.6 webtube

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require('express');
+import express from "express";
+//babel 사용해서 최신 문법으로 바꿔줌 package.json 도 건들임.
+
+//const express = require('express');
+//이건 babel 쓰기 전 상태..
+
 // require 는 node module 어딘가에서 가져오는거야
 // express 라는 파일에서 찾으려고함 못찾으면 node_modules 폴더에서 찾는다  
 const app = express();
@@ -17,15 +22,15 @@ function handleHome(req, res){
     //두개 req , res 를 호출함
     //정보를 얻고싶으면 누가 페이지에 접근했나 -> req object 사용
     //res 는 서버로부터 의 응답
-    res.send("hello from home");
+    res.send("Hello from home");
     //요렇게 해주면 리퀘스트도 하고 응답도 해줌!
 }
 
-function handleProfile(req, res){
-    res.send("You are on my profile");
-    //localhost:4000/profile
-    //서버에서 잘 받는걸 확인할수 있음. 진짜 작동하는 서버처럼 만들려면 send() 안에 html 이나 css 를 보내주는 걸 적어야함 ex)database
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
+    //funtion 을 섹시하게 쓰는법.
+//localhost:4000/profile
+//서버에서 잘 받는걸 확인할수 있음. 진짜 작동하는 서버처럼 만들려면 send() 안에 html 이나 css 를 보내주는 걸 적어야함 ex)database
+
 
 app.get("/", handleHome);
 //이것만 있으면 계속 로딩 get request 에 대한 응답이 있어야함.
@@ -35,4 +40,4 @@ app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
 // 4000개의 포트를 앱이 들어라 라고 하는거임 리슨하기 시작하면 handleListening 함수 실행하라.
 
-// hello world!
+// hello world!!
