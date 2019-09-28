8deb21d614226892ccbb970a87618b7340e51b9e
채원부 <chaeoneboo@gmail.com>
2019-09-28 14:59:28 +0900

    #3.2 MongoDB connection

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 //Application(app.js) 에서 app파일을 가져와서 이렇게 쓸수 있는거다
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
  console.log(`♥️ Listening on: http://localhost:${PORT}`);
