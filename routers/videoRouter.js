4e2b6eb2ee31b6e49ec669ec7fbc49e80120b53b
채원부 <chaeoneboo@gmail.com>
2019-10-07 23:54:23 +0900

    #3.9 Editing a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,19 +6,27 @@ import {
     getUpload,
     postUpload,
     videoDetail,
-    editVideo,
-    deleteVideo
+    deleteVideo,
+    getEditVideo,
+    postEditVideo
   } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 //밖에서 라우터들을 사용하게 하기위해 밖으로 보내는 export 를 앞에 붙임.
 const videoRouter = express.Router();
 
-
+//Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+//Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 //export default 는 파일로 export 한다는것
